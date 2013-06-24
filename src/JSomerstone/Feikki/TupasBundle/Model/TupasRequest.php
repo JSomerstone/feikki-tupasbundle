34f9e9a2cc3ddc5591dfcd583e5194db65d65470
Joona Somerkivi <joona@somerkivi.net>
2013-06-24 23:31:43 +0300

    TupasRequest has all needed setters/getters and algorithm support

--- src/JSomerstone/Feikki/TupasBundle/Model/TupasRequest.php
+++ src/JSomerstone/Feikki/TupasBundle/Model/TupasRequest.php
@@ -2,7 +2,7 @@
 
 namespace JSomerstone\Feikki\TupasBundle\Model;
 
-use Symfony\Component\Validator\Exception\InvalidArgumentException;
+use \InvalidArgumentException;
 
 class TupasRequest
 {
@@ -15,8 +15,8 @@ class TupasRequest
     private $returnLink;
     private $cancelLink;
     private $rejectionLink;
-    private $keyVersion;
-    private $algorithm;
+    private $keyVersion = 1;
+    private $algorithm = 'sha256';
     private $secret;
     private $mac;
 
@@ -31,4 +31,151 @@ class TupasRequest
     public function getVersion(){
         return $this->version;
     }
+
+    public function setServiceProvider($serviceProvider)
+    {
+        $this->serviceProvider = $serviceProvider;
+        return $this;
+    }
+
+    public function getServiceProvider(){
+        return $this->serviceProvider;
+    }
+
+    public function setLanguage($language)
+    {
+        $this->language = $language;
+        return $this;
+    }
+
+    public function getLanguage(){
+        return $this->language;
+    }
+
+    public function setRequestId($requestID){
+        $this->stamp = sprintf(
+            '%s%s',
+            date('Ymdhis'),
+            str_pad($requestID, 6, '0', STR_PAD_LEFT)
+        );
+        return $this;
+    }
+
+    public function getStamp(){
+        return $this->stamp;
+    }
+
+    public function setIdType($idType)
+    {
+        $this->idType = $idType;
+        return $this;
+    }
+
+    public function getIdType(){
+        return $this->idType;
+    }
+
+    public function setReturnLink($link)
+    {
+        $this->returnLink = $link;
+        return $this;
+    }
+
+    public function getReturnLink()
+    {
+        return $this->returnLink;
+    }
+
+    public function setCancelLink($link)
+    {
+        $this->cancelLink = $link;
+        return $this;
+    }
+
+    public function getCancelLink()
+    {
+        return $this->cancelLink;
+    }
+
+    public function setRejectedLink($link)
+    {
+        $this->rejectionLink = $link;
+        return $this;
+    }
+
+    public function getRejectedLink()
+    {
+        return $this->rejectionLink;
+    }
+
+    public function setKeyVersion($version)
+    {
+        $this->keyVersion = $version;
+        return $this;
+    }
+
+    public function getKeyVersion()
+    {
+        return $this->keyVersion;
+    }
+
+    public function setAlgorithm($alg)
+    {
+        $this->algorithm = $alg;
+        return $this;
+    }
+
+    public function getAlgorithm()
+    {
+        return $this->algorithm;
+    }
+
+    public function setSecret($secret)
+    {
+        $this->secret = $secret;
+        return $this;
+    }
+
+    public function getMac()
+    {
+        return $this->calculateMac();
+    }
+
+    private function calculateMac()
+    {
+        $array = array(
+            $this->action,
+            str_pad($this->version, 4, '0', STR_PAD_LEFT),
+            $this->serviceProvider,
+            $this->language,
+            $this->stamp,
+            $this->idType,
+            $this->returnLink,
+            $this->cancelLink,
+            $this->rejectionLink,
+            str_pad($this->keyVersion, 4, '0', STR_PAD_LEFT),
+            self::codeForAlgorithm($this->algorithm),
+            $this->secret,
+            ''
+        );
+        $string = implode('&', $array);
+
+        return hash($this->algorithm, $string);
+    }
+
+    private static function codeForAlgorithm($algorithm)
+    {
+        switch ($algorithm)
+        {
+            default:
+                throw new \InvalidArgumentException("Unsupported algorithm '$algorithm'");
+            case 'md5':
+                return '01';
+            case 'sha1':
+                return '02';
+            case 'sha256':
+                return '03';
+        }
+    }
+
 }
--- src/JSomerstone/Feikki/TupasBundle/Tests/Model/TupasRequestTest.php
+++ src/JSomerstone/Feikki/TupasBundle/Tests/Model/TupasRequestTest.php
@@ -5,6 +5,9 @@ namespace JSomerstone\Feikki\TupasBundle\Tests\Model;
 use JSomerstone\Feikki\TupasBundle\Model\TupasRequest;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * @group model
+ */
 class TupasRequestTest extends WebTestCase
 {
     protected $request;
@@ -24,10 +27,99 @@ class TupasRequestTest extends WebTestCase
     /**
      * @test
      * @dataProvider invalidVersionProvider
-     * @expectedException Symfony\Component\Validator\Exception\InvalidArgumentException
+     * @expectedException \InvalidArgumentException
      */
     public function setVersionThrowsExceptionOnInvalid($invalidVersion)
     {
        $this->request->setVersion($invalidVersion);
+    }
+
+    public function setterAndGetterProvider()
+    {
+        return array(
+            //[NameOfProperty, validValue]
+            array('ServiceProvider',    'totallyfakeone'),
+            array('Version',            1),
+            array('Language',           'FI'),
+            array('IdType',             '02'),
+            array('ReturnLink',         'http://feikki.fi/dev/null'),
+            array('CancelLink',         'http://feikki.fi/dev/null'),
+            array('RejectedLink',       'http://feikki.fi/dev/null'),
+            array('KeyVersion',         1234),
+            array('Algorithm',          'md5'),
+        );
+
+    }
+
+    /**
+     *
+     * @test
+     * @dataProvider setterAndGetterProvider
+     */
+    public function setterAndGetterWorks($property, $validValue)
+    {
+        $setter = "set$property";
+        $getter = "get$property";
+        $this->assertInstanceOf(
+            'JSomerstone\Feikki\TupasBundle\Model\TupasRequest',
+            $this->request->$setter($validValue)
+        );
+        $this->assertEquals(
+            $validValue,
+            $this->request->$getter()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function setRequestIdSetsStamp()
+    {
+        $this->request->setRequestId(123);
+        $timestamp = date('Ymdhis');
+        $this->assertSame($timestamp . '000123', $this->request->getStamp());
+
+        $this->assertRegExp(
+            '/^20[\d]{2}[01]\d[0-3]\d[0-2]\d[0-6]\d[0-6]\d000123$/',
+            $this->request->getStamp()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function hashCalculationWorks()
+    {
+        $action = '701';
+        $version = '2';
+        $serviceProvider = 'feikkidotfi';
+        $language = 'FI';
+        $requestId = '667';
+        $stamp = date('Ymdhis') . "000$requestId";
+        $idType = '01';
+        $returnLink = 'http://feikki.fi/dev/null';
+        $cancelLink = 'http://feikki.fi/dev/null';
+        $rejectedLink = 'http://feikki.fi/dev/null';
+        $keyVersion = 1;
+        $algcode = '03'; // = sha-256
+        $secret = 'S3cr370fF31kk1dotf1';
+
+        $this->request->setVersion($version)
+            ->setServiceProvider($serviceProvider)
+            ->setLanguage($language)
+            ->setRequestId($requestId)
+            ->setIdType($idType)
+            ->setReturnLink($returnLink)
+            ->setCancelLink($cancelLink)
+            ->setRejectedLink($rejectedLink)
+            ->setKeyVersion($keyVersion)
+            ->setAlgorithm('sha256')
+            ->setSecret($secret);
+
+        $string = "$action&000$version&$serviceProvider&$language&$stamp&$idType&$returnLink&$cancelLink&$rejectedLink"
+                . "&000$keyVersion&$algcode&$secret&";
+        $expectedHash = hash('sha256', $string);
+
+        $this->assertEquals($expectedHash, $this->request->getMac());
     }
 }
